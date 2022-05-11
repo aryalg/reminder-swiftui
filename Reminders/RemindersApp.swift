bcca7f9726f45e4caa49fd019708c6f6752fe885
Bikram Aryal <bikram.aryall@gmail.com>
2022-05-11 23:59:56 +0545

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Bikram Aryal on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
